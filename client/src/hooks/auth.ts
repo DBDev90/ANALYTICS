a1b5edd1036d5028f2c5c0859efd3edca0def757
DBDev90 <bordinassidouglas@gmail.com>
2024-07-01 21:34:40 -0300

    feat: criando hook de autenticacao

---
+++ client/src/hooks/auth.ts
@@ -0,0 +1,75 @@
+import { User } from "../@types/Auth";
+import { useAppDispatch } from "../redux/hooks";
+import {
+  setAuthStatus,
+  setAuthToken,
+  setUser,
+} from "../redux/slices/authSlice";
+import { getUser, login } from "../services/requests";
+
+const LOCAL_STORAGE_KEY = import.meta.env.VITE_LOCAL_STORAGE_AUTH_KEY;
+
+export const useAuth = () => {
+  const dispatch = useAppDispatch();
+
+  //Função para autenticar usuário
+  const authenticate = (user: User, authToken: string) => {
+    dispatch(setUser(user));
+    dispatch(setAuthToken(authToken));
+    dispatch(setAuthStatus("authenticated"));
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, authToken);
+  };
+
+  //Função para pegar o token do local storage
+  const handleGetToken = () => localStorage.getItem(LOCAL_STORAGE_KEY);
+
+  //Função para pegar o usuário usando o authToken salvo no local storage
+  const handleAuthenticateUser = async () => {
+    const request = await getUser();
+    const authToken = handleGetToken();
+
+    if (!request.data || !authToken) {
+      dispatch(setAuthStatus("not_authenticated"));
+      return;
+    }
+
+    const { data } = request;
+    authenticate(data.user, authToken);
+  };
+
+  //Função para login
+  const handleLogin = async ({
+    user,
+    password,
+  }: {
+    user: string;
+    password: string;
+  }) => {
+    const request = await login(user, password);
+
+    if (request.data) {
+      const { data } = request;
+      authenticate(data.user, data.authToken);
+      return true;
+    }
+
+    dispatch(setAuthStatus("not_authenticated"));
+  };
+
+  //Função para logout
+  const handleLogout = () => {
+    dispatch(setUser(null));
+    dispatch(setAuthToken(null));
+    dispatch(setAuthStatus("not_authenticated"));
+
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+  };
+
+  return {
+    handleGetToken,
+    handleAuthenticateUser,
+    handleLogin,
+    handleLogout,
+  };
+};
