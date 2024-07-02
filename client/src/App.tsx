8bcdf77006be93ab15a0ffad6f256ffeae0bf893
DBDev90 <bordinassidouglas@gmail.com>
2024-07-02 09:35:29 -0300

    feat: criando e configurando hook de useTheme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import { useEffect } from "react";
 import { useAuth } from "./hooks/auth";
+import { useTheme } from "./hooks/theme";
 
 const App = () => {
   const { handleAuthenticateUser } = useAuth();
+  const { handleInitiTheme } = useTheme();
 
   useEffect(() => {
     //Autentica usuário utilizando o token salvo no local storage;
     handleAuthenticateUser();
-  }, [handleAuthenticateUser]);
+
+    //Aplicar o tema salvo pelo usuário no local storage
+    handleInitiTheme();
+  }, [handleAuthenticateUser, handleInitiTheme]);
 
   return <></>;
 };
